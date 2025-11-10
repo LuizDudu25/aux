2c586f42f2cd9567eda13d4ca7e77dda6fb99a8f
LuizDudu25 <146662194+LuizDudu25@users.noreply.github.com>
2025-11-10 15:58:39 -0300

    Reformat geometria.py with consistent indentation

--- geometria.py
+++ geometria.py
@@ -4,14 +4,11 @@ from shapely.geometry import Point, Polygon, LineString
 def dist(p1, p2):
     return math.hypot(p2[0] - p1[0], p2[1] - p1[1])
 
-
 def ponto_dentro_poligono(p, poligono):
     return Polygon(poligono).covers(Point(p))
 
-
 def intersecao(p1, q1, p2, q2):
     return LineString([p1, q1]).intersects(LineString([p2, q2]))
-
 
 def linha_livre(p1, p2, obstaculos):
     linha = LineString([p1, p2])
